fa5cfbef65c9f2875b3aae88cdeb6291c355ceb1
KurageBurger <56003969+KurageBurger@users.noreply.github.com>
2020-03-12 15:06:53 +0900

    ヘッダーとフッターを実装

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="footer">
+    <button class="button button--link">
+      ログアウト
+    </button>
+    <RouterLink class="button button--link" to="/login">
+      ログイン / 登録
+    </RouterLink>
+  </footer>
+</template>
