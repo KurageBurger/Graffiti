cefa02fb1009dd23de5b37f13edac9cc79790719
KurageBurger <56003969+KurageBurger@users.noreply.github.com>
2020-03-14 14:37:28 +0900

    更新した際にログアウトしたように見えるのを修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,16 @@ import router from './router'
 import store from './store'
 import Vue from 'vue'
 
-var app = new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App />'
-})
+const createApp = async () => {
+  await store.dispatch('auth/currentUser')
+
+  var app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App />'
+  })
+}
+
+createApp()
