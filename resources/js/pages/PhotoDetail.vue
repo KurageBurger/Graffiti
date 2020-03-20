764a58056326b9d44edc4f21491dbd629549f4d5
KurageBurger <56003969+KurageBurger@users.noreply.github.com>
2020-03-21 02:15:24 +0900

    ポストの詳細ページの実装

--- resources/js/pages/PhotoDetail.vue
+++ resources/js/pages/PhotoDetail.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1>画像詳細ページ</h1>
+  <div
+    v-if="photo"
+    class="photo-detail"
+    :class="{ 'photo-detail--column': fullWidth }"
+  >
+    <figure
+      class="photo-detail__pane photo-detail__image"
+      @click="fullWidth = ! fullWidth"
+    >
+      <img :src="photo.url" alt="">
+      <figcaption>Posted by {{ photo.owner.name }}</figcaption>
+    </figure>
+    <div class="photo-detail__pane">
+      <button class="button button--like" title="Like photo">
+        <i class="icon ion-md-heart"></i>12
+      </button>
+      <h2 class="photo-detail__title">
+        <i class="icon ion-md-chatboxes"></i>Comments
+      </h2>
+    </div>
+  </div>
 </template>
+
+<script>
+import { OK } from '../util'
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      photo: null,
+      fullWidth: false
+    }
+  },
+  methods: {
+    async fetchPhoto () {
+      const response = await axios.get(`/api/photos/${this.id}`)
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+
+      this.photo = response.data
+    }
+  },
+  watch: {
+    $route: {
+      async handler () {
+        await this.fetchPhoto()
+      },
+      immediate: true
+    }
+  }
+}
+</script>
